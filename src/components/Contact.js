4dafce91dba7b4eefcb9afb01922a9daa716149c
nikgup6 <nikunj.gupta_2026@woxsen.edu.in>
2025-07-17 00:46:07 +0530

    Backend on render successful

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
     try {
       // Ensure this URL matches your backend.
       // If deployed, replace 'http://localhost:5000' with your backend's domain.
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://personal-portfolio-2tio.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
